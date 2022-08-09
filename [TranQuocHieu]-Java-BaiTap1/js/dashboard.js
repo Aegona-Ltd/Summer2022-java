26c3839b8955f068dfdca4f66b85117ec8d18151
Trần Quốc Hiếu <hieutran160120@gmail.com>
2022-08-09 17:03:44 +0700

    Add new charts

--- [TranQuocHieu]-Java-BaiTap1/js/dashboard.js
+++ [TranQuocHieu]-Java-BaiTap1/js/dashboard.js
@@ -1,8 +1,8 @@
 
 // Chart bar
-const cvsBar = document.getElementById('chartLine').getContext('2d');
-const chartBar = new Chart(cvsBar, {
-    type: 'bar',
+const cvsLine = document.getElementById('chartLine').getContext('2d');
+const chartLine = new Chart(cvsLine, {
+    type: 'line',
     data: {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
         datasets: [{
@@ -79,7 +79,6 @@ const chartDoughtnut = new Chart(cvsDoughnut, {
         plugins: {
             datalabels: {
                 formatter: (value, context) => {
-                    console.log(context.chart.data.datasets[0].data)
                     const datapoints = context.chart.data.datasets[0].data;
                     function totalSum(total, datapoint) {
                         return total + datapoint
@@ -96,3 +95,176 @@ const chartDoughtnut = new Chart(cvsDoughnut, {
     },
     plugins: [ChartDataLabels],
 });
+// 
+const cvsBar = document.getElementById('chartBar').getContext('2d');
+const charBar = new Chart(cvsBar, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [{
+            label: 'Số lượng',
+            data: [10, 42, 35, 78, 60, 91, 32, 57, 58, 32, 32, 77],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)'
+            ],
+            borderColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)'
+            ],
+            borderWidth: 2
+        }]
+    },
+    plugins: [ChartDataLabels],
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            }
+        },
+        plugins: {
+            datalabels: {
+                align: 'end',
+                anchor: 'end'
+            }
+        }
+    }    
+});
+
+// 
+const cvsPolar = document.getElementById('chartPolar');
+const charPolar = new Chart(cvsPolar, {
+    type: 'polarArea',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734,784,433]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
+// 
+new Chart(document.getElementById("bar-chart-grouped"), {
+    type: 'bar',
+    data: {
+      labels: ["1900", "1950", "1999", "2050"],
+      datasets: [
+        {
+          label: "Africa",
+          backgroundColor: "#3e95cd",
+          data: [133,221,783,2478]
+        }, {
+          label: "Europe",
+          backgroundColor: "#8e5ea2",
+          data: [408,547,675,734]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Population growth (millions)'
+      }
+    }
+});
+// 
+new Chart(document.getElementById("mixed-chart"), {
+    type: 'bar',
+    data: {
+      labels: ["1900", "1950", "1999", "2050"],
+      datasets: [{
+          label: "Europe",
+          type: "line",
+          borderColor: "#8e5ea2",
+          data: [408,547,675,734],
+          fill: false
+        }, {
+          label: "Africa",
+          type: "line",
+          borderColor: "#3e95cd",
+          data: [133,221,783,2478],
+          fill: false
+        }, {
+          label: "Europe",
+          type: "bar",
+          backgroundColor: 'rgb(255, 205, 86)',
+          data: [408,547,675,734],
+        }, {
+          label: "Africa",
+          type: "bar",
+          backgroundColor: 'rgb(201, 203, 207)',
+          backgroundColorHover: "#3e95cd",
+          data: [133,221,783,2478]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Population growth (millions): Europe & Africa'
+      },
+      legend: { display: false }
+    }
+});
+
+// 
+
+new Chart(document.getElementById("line-chart"), {
+    type: 'line',
+    data: {
+      labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+      datasets: [{ 
+          data: [86,114,106,106,107,111,133,221,783,2478],
+          label: "Africa",
+          borderColor: "#3e95cd",
+          fill: false
+        }, { 
+          data: [282,350,411,502,635,809,947,1402,3700,5267],
+          label: "Asia",
+          borderColor: "#8e5ea2",
+          fill: false
+        }, { 
+          data: [168,170,178,190,203,276,408,547,675,734],
+          label: "Europe",
+          borderColor: "#3cba9f",
+          fill: false
+        }, { 
+          data: [40,20,10,16,24,38,74,167,508,784],
+          label: "Latin America",
+          borderColor: "#e8c3b9",
+          fill: false
+        }, { 
+          data: [6,3,2,2,7,26,82,172,312,433],
+          label: "North America",
+          borderColor: "#c45850",
+          fill: false
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'World population per region (in millions)'
+      }
+    }
+  });
+  
